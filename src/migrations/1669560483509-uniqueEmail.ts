16ddab325eea87bb9fa310d26dad318afcf8db1c
Zain Alwan Wima Irfani <zainalwan4@gmail.com>
2022-11-27 21:57:03 +0700

    Generate unique email migration

---
+++ src/migrations/1669560483509-uniqueEmail.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class uniqueEmail1669560483509 implements MigrationInterface {
+    name = 'uniqueEmail1669560483509'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email")`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3"`);
+    }
+
+}
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,7 @@
 import { init1669519821659 } from './1669519821659-init';
+import { uniqueEmail1669560483509 } from './1669560483509-uniqueEmail';
 
 export const migrations = [
   init1669519821659,
+  uniqueEmail1669560483509,
 ];
